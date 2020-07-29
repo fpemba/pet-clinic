2dcbc3afce8f5c62140dd94e7f6571ca62aacb6e
fpemba <fatuma.pemba@yahoo.com>
2020-07-29 19:56:26 -0400

    added specialities, added to vet. Closes #28.

--- src/main/java/com/fpemba/petclinic/models/Vet.java
+++ src/main/java/com/fpemba/petclinic/models/Vet.java
@@ -1,5 +1,16 @@
 package com.fpemba.petclinic.models;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Specialty> specialities;
+
+    public Set<Specialty> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Specialty> specialities) {
+        this.specialities = specialities;
+    }
 }
