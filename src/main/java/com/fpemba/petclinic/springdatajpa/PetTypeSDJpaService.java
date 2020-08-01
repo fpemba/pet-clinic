7f803e358f8588f768b76d6c70d1cfe1166b922e
fpemba <fatuma.pemba@yahoo.com>
2020-08-01 14:55:11 -0400

    added Spring data jpa for PetType. Closes #43.

---
+++ src/main/java/com/fpemba/petclinic/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,49 @@
+package com.fpemba.petclinic.springdatajpa;
+
+import com.fpemba.petclinic.models.PetType;
+import com.fpemba.petclinic.repositories.PetTypeRepository;
+import com.fpemba.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
