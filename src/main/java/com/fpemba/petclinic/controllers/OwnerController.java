2d5880a7c0f17186c8ae830f598a38ffee26e713
fpemba <fatuma.pemba@yahoo.com>
2020-10-04 22:03:45 -0400

    added DisplayOwner records. Closes #54.

--- src/main/java/com/fpemba/petclinic/controllers/OwnerController.java
+++ src/main/java/com/fpemba/petclinic/controllers/OwnerController.java
@@ -4,7 +4,9 @@ import com.fpemba.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -17,14 +19,21 @@ public class OwnerController {
     }
 
     @GetMapping({"","/","/index","/index.html"})
-    public String listOwner(Model model){
+    public String listOwner(Model model) {
 
-        model.addAttribute("owners",ownerService.findAll());
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
     @GetMapping("/find")
-    public String findOwners(){
+    public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
