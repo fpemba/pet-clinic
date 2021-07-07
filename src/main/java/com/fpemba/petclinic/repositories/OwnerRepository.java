2ee64e3818aadcc832bc18fabce5f9e5f9c9bada
fpemba <fatuma.pemba@yahoo.com>
2021-07-07 15:47:55 -0400

    implement find owner. closes #54

--- src/main/java/com/fpemba/petclinic/repositories/OwnerRepository.java
+++ src/main/java/com/fpemba/petclinic/repositories/OwnerRepository.java
@@ -3,7 +3,12 @@ package com.fpemba.petclinic.repositories;
 import com.fpemba.petclinic.models.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudRepository<Owner,Long> {
+import java.util.List;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
+
 }
--- src/main/java/com/fpemba/petclinic/services/maps/OwnerMapService.java
+++ src/main/java/com/fpemba/petclinic/services/maps/OwnerMapService.java
@@ -8,6 +8,7 @@ import com.fpemba.petclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -82,5 +83,11 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                 .orElse(null);
 
 
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        //todo - impl
+        return null;
     }
 }
